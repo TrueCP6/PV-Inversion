92f9b9df743d4b63221a5fdde2f44cd954c83b43
truecp5 <truecp5@gmail.com>
2026-06-23 15:18:19 -0400

    Write formal test for MMS

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
 import unittest
+from domain_builder import *
 from math_utils import *
+from parameters import *
+from solver import *
+from mms_checker import *
 
-class MyTestCase(unittest.TestCase):
+class UtilTests(unittest.TestCase):
     def test_vertical_integral(self):
         mesh2d = UnitSquareMesh(10, 10, quadrilateral=True)
         mesh = ExtrudedMesh(mesh2d, layers=10, layer_height=0.1)
@@ -20,6 +24,29 @@ class MyTestCase(unittest.TestCase):
         for integrand, exact in test_cases:
             num_soln = compute_vertical_integral(integrand, V)
             error = errornorm(exact, num_soln)
+            self.assertLess(error, 1e-6)
+
+class MMSTests(unittest.TestCase):
+    def test_mms(self):
+        N = 20
+        solver_params = SolverParams(
+            check_flux=False,
+            nx=N, ny=N, nz=N
+        )
+        phys_params = PhysicalParams(
+            Lx=1e6, Ly=1e6, H = 20e3
+        )
+
+        domain_builder = DomainBuilder(solver_params, phys_params)
+        mesh = domain_builder.mesh()
+        V = domain_builder.func_space()
+
+        atmos = MMSChecker(mesh, V, phys_params)
+
+        for save_memory in [True, False]:
+            solver = Solver(atmos, solver_params, save_memory)
+            solver.solve_psi()
+            error = atmos.calc_error(solver.psi_soln)
             self.assertLess(error, 1e-6)
 
 
